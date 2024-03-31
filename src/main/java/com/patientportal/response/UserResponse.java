16d847cea78d5a0cdcf4bcc7a42e0051837409a5
Armin Pajevic <apajevic@aquivalabs.com>
2024-03-31 13:56:19 +0200

    Revert "improved code"
    This reverts commit 3ce90d8639cfe800acfa8c75a4bc139a42934e02.

--- src/main/java/com/patientportal/response/UserResponse.java
+++ src/main/java/com/patientportal/response/UserResponse.java
@@ -2,14 +2,13 @@ package com.patientportal.response;
 
 import com.patientportal.model.Gender;
 import com.patientportal.model.Role;
-import lombok.AllArgsConstructor;
+import com.patientportal.model.User;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.time.LocalDateTime;
 
-@AllArgsConstructor
 @NoArgsConstructor
 @Getter
 @Setter
@@ -24,6 +23,19 @@ public class UserResponse {
     private int loginCount;
     private LocalDateTime createdAt;
     private LocalDateTime updatedAt;
+
+    public UserResponse(User user) {
+        this.id = user.getId();
+        this.name = user.getName();
+        this.email = user.getEmail();
+        this.phone = user.getPhone();
+        this.address = user.getAddress();
+        this.gender = user.getGender();
+        this.role = user.getRole();
+        this.loginCount = user.getLoginCount();
+        this.createdAt = user.getCreatedAt();
+        this.updatedAt = user.getUpdatedAt();
+    }
 }
 
 
